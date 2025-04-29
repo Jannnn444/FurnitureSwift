e38f567182af2b9dd8d7bb51fcb353c709dde743
Janus <Janus.Haung@hualiteq.com>
2025-04-29 17:06:50 +0800

    Initial Commit

---
+++ FurnitureSwift/FurnitureSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureSwiftApp.swift
+//  FurnitureSwift
+//
+//  Created by Hualiteq International on 2025/4/29.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
