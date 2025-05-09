b56de6ca07b6b6ec2ecc80f1fd602f1451a01d95
Jannnn444 <yucianhuang@gmail.com>
2025-05-09 14:27:44 +0800

    Create Product detail view

---
+++ Views/ProductDetailView.swift
@@ -0,0 +1,64 @@
+//
+//  ProductDetailView.swift
+//  FurnitureSwift
+//
+//  Created by Hualiteq International on 2025/5/9.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var product: Product
+    
+    var body: some View {
+        ScrollView {
+            ZStack {
+                Color.white
+                
+                VStack(alignment: .leading) {
+                    ZStack(alignment: .topTrailing) {
+                        Image(product.image)
+                            .resizable()
+                            .ignoresSafeArea(edges: .top)
+                            .frame(height: 300)
+                        
+                        Image(systemName: "heart.fill")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .padding(.top, 65)
+                            .padding(.trailing, 20)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(product.name)
+                                .font(.title2.bold())
+                            Spacer()
+                            
+                            Text("$\(product.price).00")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal)
+                                .background(Color.gray) //secondary
+                                .cornerRadius(12)
+                        }
+                        .padding(.vertical)
+                        HStack(spacing: 10) {
+                            ForEach(0..<5) { index in
+                                Image(systemName: "star.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .foregroundColor(.yellow)
+                            }
+                        }
+                    }
+                }
+            }
+        } .ignoresSafeArea(edges: .top)
+    }
+}
+
+#Preview {
+    ProductDetailView(product: productList[1])
+    
+}
